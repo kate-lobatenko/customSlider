60e005bd0739dcfe7bdabc520a9bfbf5ab104670
kate-lobatenko <ttt1232444@gmail.com>
2019-05-10 18:20:17 +0300

    changed css, html and js for base application state

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 window.addEventListener('load', function () {
-    new init;
+    new initSlider;
 });
-
-function init() {
-    initSlider();
-}
 
 function initSlider() {
     drawSlider();
@@ -17,5 +13,13 @@ function drawSlider() {
 }
 
 function loadEventsListeners() {
+    const $leftControl = document.querySelector("[data-click='left']");
+    const $rightControl = document.querySelector("[data-click='right']");
+    $leftControl.addEventListener("click", contolsClick);
+    $rightControl.addEventListener("click", contolsClick);
+}
 
+function contolsClick() {
+    console.log("click on control");
+    this.style.transform = "translateX(450)";
 }
